e9f376e48a782b3dfee11d029ff6d489c4a1950f
Rafildsks <rafaelfelipes111@gmail.com>
2026-05-07 21:49:49 -0300

    Primeiros passos do JavaScript

---
+++ index.js
@@ -0,0 +1,28 @@
+// Métodos de Saida
+alert("Hello Word!");
+
+// Métodos de Entrada
+confirm("Deseja receber notificações?");
+
+// Métodos de Dados
+
+// "Rafael" - String;
+// 22, NaN - Number;
+// lef fruta - undefined;
+// null;
+// true/false -Boolean;
+
+// Variáveis;
+
+let turma = "FS63";
+console.log(turma);
+
+turma = "FS62";
+console.log(turma);
+
+// posso declarar e atribuir valor depois
+
+let aula
+console.log(aula)
+
+const pessoa // erro precisa atribuir valor
